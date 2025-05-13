0bf1f04bfa124a8edf39237d9d990e14b23657c4
okonghw <okong1@hwemail.com>
2025-05-13 10:42:04 -0700

    added checkpoints

--- Assets/GameRespawn.cs
+++ Assets/GameRespawn.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class GameRespawn : MonoBehaviour
 {
     public float threshold;
+    public Vector3 playerPosition;
+    [SerializeField] List<GameObject> checkpoints;
+    [SerializeField] Vector3 vectorPoint;
 
     void FixedUpdate()
     {
         if (transform.position.y < threshold) {
-            transform.position = new Vector3(-0.3037853f, 2.1f, -10.92337f);
+            // transform.position = new Vector3(-0.3037853f, 2.1f, -10.92337f);
+            transform.position = new Vector3(playerPosition.x, playerPosition.y, playerPosition.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            vectorPoint = other.transform.position;
+            playerPosition = vectorPoint;
         }
     }
 }
