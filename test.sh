4982c7ead5adbd29129899571a35217831ad0655
Michael Barr <61331006+fishsticks89@users.noreply.github.com>
2025-05-23 00:59:03 -0700

    the game be so done

---
+++ test.sh
@@ -0,0 +1,3 @@
+./KartKlash.app/Contents/MacOS/My\ project -client -ip 127.0.0.1 -port 7777 &
+./KartKlash.app/Contents/MacOS/My\ project -client -ip 127.0.0.1 -port 7777 &
+./KartKlash.app/Contents/MacOS/My\ project -server -port 7777
